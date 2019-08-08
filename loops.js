891682fe956b2cd8547208439e0c0f0490e09a83
Hope Giometti <hopegiometti@gmail.com>
2019-08-08 19:33:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,5 +28,5 @@ function incrementVariable(){
 function doWhileLoop(i) {
   do {
     console.log(`I run once regardless.`);
-  } while (incrementVariable() < 10);
+  } while (incrementVariable(i) < 10);
 }
