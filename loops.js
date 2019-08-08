95da28be6a3b78c73c152d7dd062187a2aaeae8b
Hope Giometti <hopegiometti@gmail.com>
2019-08-08 18:37:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
-for (var i = 1; i < 25; i++) {
-  console.log("I am" + ${i} + "strange loops.");
+function forLoop() {
+  for (var i = 1; i < 25; i++) {
+    console.log("I am" + ${i} + "strange loops.");
+  }
 }
